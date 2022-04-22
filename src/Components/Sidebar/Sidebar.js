9cc7daaacb873f0138d15bc3f6cf9327e391ab15
Ritul Daryan <ritul.daryan.205@gmail.com>
2022-04-23 03:26:35 +0530

    Delete Button added to Workspace

--- src/Components/Sidebar/Sidebar.js
+++ src/Components/Sidebar/Sidebar.js
@@ -4,86 +4,100 @@ import { SET_ACTIVE_WORKSPACE } from '../../UserContext/actions.type';
 import { userContext } from '../../UserContext/store';
 import sty from './Sidebar.module.css';
 
+import Delete from '../../Assets/Delete.svg';
+
 const Sidebar = () => {
   const { state, dispatch } = useContext(userContext);
-  const [dummy, setDummy] = useState(['My ToDos']);
+  const [cnfDelete, setCnfDelete] = useState(false);
   const [selectedItem, setSelectedItem] = useState('My ToDos'); //defaul selected Item is Always ONE
   const [item, setItem] = useState({
     value: '',
     status: false,
   });
 
-  /* 
-TODO:
-
- replace Dummy data and Import Workspace list from firebase
-  Push New Workspace to Firebase
-  Check State for input and Selected Item are OK or NOT, 
-  use uuid indead of key in list of workspace
-*/
+  const deleteSpaceHandler = () => {
+    console.log('Workspace 01');
+    setCnfDelete(false);
+  };
 
   return (
-    <div className={sty.sidebar}>
-      <p className={sty.title}>My Workspaces</p>
-      <div className={sty.worklist}>
-        {state.workspace.map(([index, workspace]) => (
-          <div
-            key={index}
-            className={`${sty.workitem} ${
-              workspace === selectedItem ? sty.active : ''
-            }`}
-            onClick={() => {
-              setSelectedItem(workspace);
-              dispatch({
-                type: SET_ACTIVE_WORKSPACE,
-                payload: [index, workspace],
-              });
-            }}
-          >
-            {workspace}
-          </div>
-        ))}
-      </div>
-      <div className={sty.buttonBox}>
-        {!item.status ? (
-          <button
-            className={sty.button}
-            onClick={() => {
-              setItem({
-                ...item,
-                status: true,
-              });
-            }}
-          >
-            Add new Workspace
-          </button>
-        ) : (
-          <input
-            type={'text'}
-            className={`${sty.button} ${sty.input}`}
-            onChange={(event) =>
-              setItem({
-                ...item,
-                value: event.target.value,
-              })
-            }
-            value={item.value}
-            placeholder={'Enter Workspace Name'}
-            onKeyDown={(event) => {
-              if (event.key === 'Enter') {
-                if (item.value !== '') {
-                  addWorkspace(item.value, state.user.uid, dispatch);
-                }
-                setItem({
-                  value: '',
-                  status: false,
+    <>
+      {cnfDelete && (
+        <div className={sty.confirm}>
+          <p>Deleting a workspace delete all Todos and Progress</p>
+          <span>
+            <button onClick={() => deleteSpaceHandler()}>Confrim Delete</button>
+            <button onClick={() => setCnfDelete(false)}>Cancel Delete</button>
+          </span>
+        </div>
+      )}
+      <div className={sty.sidebar}>
+        <p className={sty.title}>My Workspaces</p>
+        <div className={sty.worklist}>
+          {state.workspace.map(([index, workspace]) => (
+            <div
+              key={index}
+              className={`${sty.workitem} ${
+                workspace === selectedItem ? sty.active : ''
+              }`}
+              onClick={() => {
+                setSelectedItem(workspace);
+                dispatch({
+                  type: SET_ACTIVE_WORKSPACE,
+                  payload: [index, workspace],
                 });
+              }}
+            >
+              {workspace}
+              {workspace === selectedItem && (
+                <span className={sty.icons} onClick={() => setCnfDelete(true)}>
+                  <img src={Delete} />
+                </span>
+              )}
+            </div>
+          ))}
+        </div>
+        <div className={sty.buttonBox}>
+          {!item.status ? (
+            <button
+              className={sty.button}
+              onClick={() => {
+                setItem({
+                  ...item,
+                  status: true,
+                });
+              }}
+            >
+              Add new Workspace
+            </button>
+          ) : (
+            <input
+              type={'text'}
+              className={`${sty.button} ${sty.input}`}
+              onChange={event =>
+                setItem({
+                  ...item,
+                  value: event.target.value,
+                })
               }
-            }}
-          />
-        )}
+              value={item.value}
+              placeholder={'Enter Workspace Name'}
+              onKeyDown={event => {
+                if (event.key === 'Enter') {
+                  if (item.value !== '') {
+                    addWorkspace(item.value, state.user.uid, dispatch);
+                  }
+                  setItem({
+                    value: '',
+                    status: false,
+                  });
+                }
+              }}
+            />
+          )}
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
